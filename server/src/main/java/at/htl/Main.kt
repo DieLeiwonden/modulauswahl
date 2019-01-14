9e543d1c7dcacf5abd1a400f1fadcb8a940ad7fa
Tim Untersberger <timuntersberger@Tims-Air.mshome.net>
2019-01-14 09:05:00 +0100

    Initial

--- server/src/main/java/at/htl/Main.kt
+++ server/src/main/java/at/htl/Main.kt
@@ -1,5 +1,8 @@
 package at.htl;
 
+import io.javalin.Javalin
+
 fun main(args: Array<String>){
-    println("Hello World")
+    val app = Javalin.create().start(5000)
+    app.get("/") { ctx -> ctx.result("Hello World") }
 }
